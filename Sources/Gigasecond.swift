497c80aa75aeecfb82672e8eff4bad8c07c4ac95
davidkneely <deeniri@gmail.com>
2017-05-25 19:59:22 -1000

    (share):First Commit

---
+++ Sources/Gigasecond.swift
@@ -0,0 +1,22 @@
+//Solution goes in Sources
+
+import Foundation
+
+class Gigasecond {
+    
+    var date: NSDate
+    
+    var description: String
+    
+    init(from date: String) {
+        
+        self.date = NSDate.date(from: date)
+        
+        // declare gigasecond
+        
+        
+        // add it to nsDate
+        
+        // set description
+    }
+}
